c6a142d2659cce0306939a503523c20a6515e8af
Zasonnik <zasonnik@yandex.ru>
2015-06-24 22:31:11 +0300

    Realy magic of slice

--- maps/main.go
+++ maps/main.go
@@ -3,14 +3,50 @@ package main
 import "fmt"
 
 func main(){
-	x := make(map[string]int)
-	x["key"] = 10
-	x["key1"] = 20
-	delete(x,"key")
-	v,ok := x["key1"]
-	if ok {
-		fmt.Println(v)
-	} else {
-		fmt.Println("nope")
-	}
+	elements := map[string]map[string]string{
+            "H": map[string]string{
+            "name":"Hydrogen", 
+            "state":"gas",
+        },
+        "He": map[string]string{
+            "name":"Helium", 
+            "state":"gas",
+        },
+        "Li": map[string]string{
+            "name":"Lithium", 
+            "state":"solid",
+        },
+        "Be": map[string]string{
+            "name":"Beryllium", 
+            "state":"solid",
+        },
+        "B":  map[string]string{
+            "name":"Boron",
+            "state":"solid",
+        },
+        "C":  map[string]string{
+            "name":"Carbon",
+            "state":"solid",
+        },
+        "N":  map[string]string{
+            "name":"Nitrogen",
+            "state":"gas",
+        },
+        "O":  map[string]string{
+            "name":"Oxygen",
+            "state":"gas",
+        },
+        "F":  map[string]string{
+            "name":"Fluorine",
+            "state":"gas",
+        },
+        "Ne":  map[string]string{
+            "name":"Neon",
+            "state":"gas",
+        },
+    }
+
+    if el, ok := elements["Li"]; ok {    
+        fmt.Println(el["name"], el["state"])
+    }
 }
