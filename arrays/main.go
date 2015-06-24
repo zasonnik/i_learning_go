1cf8f07d3828bbbe51efc525d5925fb9ee7bf07f
Zasonnik <zasonnik@yandex.ru>
2015-06-24 21:58:47 +0300

    for and array!

--- arrays/main.go
+++ arrays/main.go
@@ -2,7 +2,16 @@ package main
 
 import "fmt"
 func main() {
-	var x [5]int
-	x[4] = 100
-	fmt.Println(x)
+	var x [5]float64
+	x[0] = 54
+	x[1] = 77
+	x[2] = 81
+	x[3] = 98
+	x[4] = 51
+
+	var total float64 = 0
+	for i:=0; i<len(x); i++ {
+		total +=x[i]
+	}
+	fmt.Println(total/float64(len(x)))
 }
