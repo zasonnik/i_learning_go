e6442a4bf20083a7023e3e9b4655bb53995d34b7
Zasonnik <zasonnik@yandex.ru>
2015-06-25 23:30:07 +0300

    defer

--- funct/main.go
+++ funct/main.go
@@ -3,19 +3,16 @@ package main
 
 import "fmt"
 
-func makeEvenGenerator() func() uint{
-	i:= uint(0)
-	return func() (ret uint) {
-		ret = i
-		i += 2
+func first() {
+	fmt.Println("1st")
+}
 
-		return ret
-	}
+func second() {
+	fmt.Println("2st")
 }
 
 func main() {
-	nextEven := makeEvenGenerator()
-	fmt.Println(nextEven())
-	fmt.Println(nextEven())
-	fmt.Println(nextEven())
+	defer second()
+	first()
+	fmt.Println("middle")
 }
