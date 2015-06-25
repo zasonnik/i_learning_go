73cb8ecbac82dec6fa8e4cfd281d1262fa144850
Zasonnik <zasonnik@yandex.ru>
2015-06-25 23:20:32 +0300

    closure

--- funct/main.go
+++ funct/main.go
@@ -3,15 +3,12 @@ package main
 
 import "fmt"
 
-func average(xs []float64) float64 {
-	total := 0.0
-	for _, v := range xs{
-		total += v
-	}
-	return total / float64(len(xs))
-}
-
 func main() {
-	xs := []float64{98,93,77,82,83}
-	fmt.Println(average(xs))
+	x := 0
+	increment := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
 }
