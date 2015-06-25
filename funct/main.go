10860974c2cc8ab4f8a1afc58a1101c1d515df90
Zasonnik <zasonnik@yandex.ru>
2015-06-25 23:26:43 +0300

    Cool closure!

--- funct/main.go
+++ funct/main.go
@@ -3,12 +3,19 @@ package main
 
 import "fmt"
 
-func main() {
-	x := 0
-	increment := func() int {
-		x++
-		return x
+func makeEvenGenerator() func() uint{
+	i:= uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+
+		return ret
 	}
-	fmt.Println(increment())
-	fmt.Println(increment())
+}
+
+func main() {
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
 }
