8e7d901b759a4cca5aa00c2ca5fce2b98aa1416a
Zasonnik <zasonnik@yandex.ru>
2015-06-28 01:52:52 +0300

    My first interface!
    Remember: Interfacies can be fields of another structs!

--- strunctsAndInterfacies/main.go
+++ strunctsAndInterfacies/main.go
@@ -1,21 +1,39 @@
 package main
 
-import "fmt"
+import ("fmt"; "math")
 
-type Person struct {
-	Name string
+type Circle struct {
+	x, y, r float64
 }
 
-func (p *Person) Talk(){
-	fmt.Println("Hi, my name is", p.Name)
+func (c *Circle) area() float64 {
+	return math.Pi * c.r*c.r
 }
 
-type Android struct {
-	Person
-	Model string
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := math.Abs(r.x1-r.x2)
+	w := math.Abs(r.y1-r.y2)
+	return l * w
+}
+
+type Shape interface {
+	area() float64
+}
+
+func totalArea(shapes ...Shape) float64 {
+	var area float64
+	for _, s := range shapes {
+		area += s.area()
+	}
+	return area
 }
 
 func main() {
-	c := Android{Person{"John"},"C11"}
-	c.Talk()
+	c := Circle{0, 0, 5}
+	r := Rectangle{0,0,3,4}
+	fmt.Println(totalArea(&c,&r))
 }
