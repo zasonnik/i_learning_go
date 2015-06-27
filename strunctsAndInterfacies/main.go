babe3c72e6eade0e831ac552c8b5403dc7e92ca5
Zasonnik <zasonnik@yandex.ru>
2015-06-28 01:25:54 +0300

    One more interface

--- strunctsAndInterfacies/main.go
+++ strunctsAndInterfacies/main.go
@@ -10,7 +10,19 @@ func (c *Circle) area() float64 {
 	return math.Pi * c.r*c.r
 }
 
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := math.Abs(r.x1-r.x2)
+	w := math.Abs(r.y1-r.y2)
+	return l * w
+}
+
 func main() {
 	c := Circle{0, 0, 5}
+	r := Rectangle{0,0,3,4}
 	fmt.Println(c.area())
+	fmt.Println(r.area())
 }
