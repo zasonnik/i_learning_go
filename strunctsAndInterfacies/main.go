c7342126a7f448913db87ed44ee372d37a7e0269
Zasonnik <zasonnik@yandex.ru>
2015-06-28 01:06:45 +0300

    Method

--- strunctsAndInterfacies/main.go
+++ strunctsAndInterfacies/main.go
@@ -6,17 +6,11 @@ type Circle struct {
 	x, y, r float64
 }
 
-func circleArea(c Circle) float64 {
+func (c *Circle) area() float64 {
 	return math.Pi * c.r*c.r
 }
 
 func main() {
-	var one Circle
-	two := new(Circle)
-	three := Circle{x: 0, y: 0, r: 5}
-	four := Circle{0, 0, 5}
-	fmt.Println(circleArea(one))
-	fmt.Println(circleArea(*two))
-	fmt.Println(circleArea(three))
-	fmt.Println(circleArea(four))
+	c := Circle{0, 0, 5}
+	fmt.Println(c.area())
 }
