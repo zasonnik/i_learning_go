1ccd8b5ec834b3baae7010625294acfdafdb18e4
Zasonnik <zasonnik@yandex.ru>
2015-06-28 19:36:00 +0300

    buffers chans

--- multithreads/chanels/main.go
+++ multithreads/chanels/main.go
@@ -2,25 +2,25 @@ package main
 
 import (
 	"fmt"
-	"time"
+//	"time"
 )
 
 
 func main() {
-	c1 := make(chan string)
-	c2 := make(chan string)
+	c1 := make(chan string, 3)
+	c2 := make(chan string, 5)
 
 	go func() {
 		for {
 			c1 <- "from 1"
-			time.Sleep(time.Second * 2)
+	//		time.Sleep(time.Second * 2)
 		}
 	}()
 
 	go func() {
 		for {
 			c2 <- "from 2"
-			time.Sleep(time.Second * 3)
+	//		time.Sleep(time.Second * 3)
 		}
 	}()
 
